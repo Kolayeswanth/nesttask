060ab91c7151d95ee719ad42a46289c0c2109ed8
yeswanth kola <143801141+Kolayeswanth@users.noreply.github.com>
2024-06-07 14:50:30 +0530

    Add files via upload

---
+++ nest1/hello-prisma/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  controllers: [AppController],
+  providers: [UserService, PostService, PrismaService],
+})
+export class AppModule {}
